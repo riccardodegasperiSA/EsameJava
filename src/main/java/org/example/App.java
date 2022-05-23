fc5c9ec9ad70b38a3a43f254a4b7a8a11a2a70e8
riccardodegasperiSA <riccardo.degasperi@itsrizzoli.it>
2022-05-23 12:36:13 +0000

    fix: add more instances

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -74,10 +74,12 @@ public class App {
     static void buildCitiesList() {
         piatti.add(new Piatto("Il tradizionale risotto allo zafferano", 1,
                 "Risotto alla Milanese",12.20));
-        piatti.add(new Piatto("La classica crostata di mele", 2,
+        piatti.add(new Piatto("La classica crostata di mele", 12,
                 "Crostata di mele",22.50));
-        piatti.add(new Piatto("Il classico primo", 2,
+        piatti.add(new Piatto("Il classico primo", 4,
                 "Spaghetti al pomodoro",7.79));
+        piatti.add(new Piatto("La famosa pizza con pomodore e mozzarella", 2,
+                "Pizza margherita",7.79));
         System.out.println(piatti);
     }
 
